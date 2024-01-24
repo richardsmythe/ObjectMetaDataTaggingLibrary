886e50dd37ecae2cf61e3e4c9a7c50df5cc42f40
Richard Smythe <richsmythe@gmail.com>
2024-01-24 13:02:58 +0000

    Made methods virtual in the facade class

--- ObjectMetaDataTagging/Services/ObjectMetaDataTaggingFacade.cs
+++ ObjectMetaDataTagging/Services/ObjectMetaDataTaggingFacade.cs
@@ -14,25 +14,25 @@ namespace ObjectMetaDataTagging
             _taggingService = taggingService ?? throw new ArgumentNullException(nameof(taggingService));
         }
 
-        public async Task SetTagAsync(object o, T tag) => await _taggingService.SetTagAsync(o, tag);
+        public virtual async Task SetTagAsync(object o, T tag) => await _taggingService.SetTagAsync(o, tag);
 
-        public async Task<bool> UpdateTagAsync(object o, Guid tagId, T newTag) => await _taggingService.UpdateTagAsync(o, tagId, newTag);
+        public virtual async Task<bool> UpdateTagAsync(object o, Guid tagId, T newTag) => await _taggingService.UpdateTagAsync(o, tagId, newTag);
 
-        public async Task<IEnumerable<T>> GetAllTags(object o) => await _taggingService.GetAllTags(o);
+        public virtual async Task<IEnumerable<T>> GetAllTags(object o) => await _taggingService.GetAllTags(o);
 
-        public async Task<T>? GetTag(object o, Guid tagId) => await _taggingService.GetTag(o, tagId);
+        public virtual async Task<T>? GetTag(object o, Guid tagId) => await _taggingService.GetTag(o, tagId);
 
-        public async Task<bool> RemoveAllTagsAsync(object o) => await _taggingService.RemoveAllTagsAsync(o);
+        public virtual async Task<bool> RemoveAllTagsAsync(object o) => await _taggingService.RemoveAllTagsAsync(o);
 
-        public async Task<bool> RemoveTagAsync(object? o, Guid tagId) => await _taggingService.RemoveTagAsync(o, tagId);
+        public virtual async Task<bool> RemoveTagAsync(object? o, Guid tagId) => await _taggingService.RemoveTagAsync(o, tagId);
 
         public bool HasTag(object o, Guid tagId) => _taggingService.HasTag(o, tagId);
 
-        public object? GetObjectByTag(Guid tagId) => _taggingService.GetObjectByTag(tagId);
+        public virtual  object? GetObjectByTag(Guid tagId) => _taggingService.GetObjectByTag(tagId);
 
         public async Task<List<GraphNode>> GetObjectGraph() => await _taggingService.GetObjectGraph();
 
-        public async Task BulkAddTagsAsync(object o, IEnumerable<T> tags) => await _taggingService.BulkAddTagsAsync(o, tags);
+        public virtual async Task BulkAddTagsAsync(object o, IEnumerable<T> tags) => await _taggingService.BulkAddTagsAsync(o, tags);
 
         public event EventHandler<AsyncTagAddedEventArgs> TagAdded
         {
