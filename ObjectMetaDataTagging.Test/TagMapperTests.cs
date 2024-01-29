cec3e2a95a954245256a317537a9fb6d9524807a
Richard Smythe <richsmythe@gmail.com>
2024-01-29 16:44:29 +0000

    Modified test for TagMapper

--- ObjectMetaDataTagging.Test/TagMapperTests.cs
+++ ObjectMetaDataTagging.Test/TagMapperTests.cs
@@ -1,6 +1,9 @@
 ﻿using ObjectMetaDataTagging.Events;
 using ObjectMetaDataTagging.Models.TagModels;
 using ObjectMetaDataTagging.Services;
+using System;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace ObjectMetaDataTagging.Test
 {
@@ -10,22 +13,30 @@ namespace ObjectMetaDataTagging.Test
         public async Task TagMapper_ShouldMapTagsFromOneTypeToAnother()
         {
             // Arrange
+            var tagMapper = new TagMapper<BaseTag, TestTagType>();
             var taggingService = new InMemoryTaggingService<BaseTag>();
-            var obj1 = new PersonTranscation { Amount = 1500, Sender = "Richard", Receiver = "Bob" };
-            var tag = new BaseTag("TestTag", "Warning", "A string tag");
-            var tagMapper = new TagMapper<TestTagType>();
+            var obj1 = new PersonTransaction { Amount = 1500, Sender = "Richard", Receiver = "Bob" };
+            var sourceTagToMap = new BaseTag("TestTag", "Warning", "A string tag");
 
-            await taggingService.SetTagAsync(obj1, tag);
+            await taggingService.SetTagAsync(obj1, sourceTagToMap);
 
             // Act
-            var mappedObject = await tagMapper.MapTagsFromOtherType(tag);
+            var mappedObject = await tagMapper.MapTagsBetweenTypes(sourceTagToMap, new TestTagType());
 
             // Assert
             Assert.NotNull(mappedObject);
-            Assert.NotEqual(mappedObject.GetType().Name, tag.GetType().Name);
-         
+            Assert.NotEqual(sourceTagToMap.GetType().Name, mappedObject.GetType().Name);
+            Assert.Equal(sourceTagToMap.Name, mappedObject.Name);
+            Assert.Equal(sourceTagToMap.Value, mappedObject.Value);
+            Assert.Equal(sourceTagToMap.Description, mappedObject.Description);
+            // Add additional assertions based on your mapping logic
+
+            // If you have specific properties in TestTagType, you can check them too
+            Assert.Equal("Test", ((TestTagType)mappedObject).TestProperty1);
+            Assert.Equal("Test2", ((TestTagType)mappedObject).TestProperty2);
         }
-        public class PersonTranscation
+
+        public class PersonTransaction
         {
             public Guid Id { get; set; } = Guid.NewGuid();
             public string Sender { get; set; }
@@ -35,20 +46,14 @@ namespace ObjectMetaDataTagging.Test
 
         public class TestTagType : BaseTag
         {
-            public string TestProperty { get; set; }
+            public string TestProperty1 { get; set; }
+            public string TestProperty2 { get; set; }
 
-            public TestTagType() : base("DefaultName", "DefaultValue")
+            public TestTagType()
             {
-                TestProperty = "Test";
-            }
-
-            public TestTagType(string name, object value, string description = "Test test test")
-                : base(name, value, description)
-            {
+                TestProperty1 = "Test";
+                TestProperty2 = "Test2";
             }
         }
-
-
     }
-
 }
